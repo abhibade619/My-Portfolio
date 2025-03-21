c0ca10999caf312706d2f8651c40b332830fbe28
abhibade619 <abhibade6@gmail.com>
2025-03-21 11:59:05 -0400

    Removed node_modules from tracking

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -22,6 +22,21 @@ const Projects = () => {
           title="Airbnb Dashboard"
           main="An interactive Tableau dashboard analyzing Airbnb listings, focusing on price trends, room types, and host verification. Presented insights to enhance decision-making."
           demoLink="https://public.tableau.com/app/profile/abhishek.bade/viz/aly6070_dashboard/Dashboard1"
+          sourceLink="https://github.com/abhibade619/Insurance-charges-prediction-project"
+        />
+        <ProjectCard
+          title="Human Activity Recognition using LSTM"
+          main="Developed an LSTM-based model to classify human activities using time-series sensor data. Implemented data preprocessing, model training, and evaluation for accurate predictions."
+          sourceLink="https://github.com/abhibade619/Human-activity-recognition-using-LSTM"
+        />
+        <ProjectCard
+          title="Power BI Dashboard - Boston Syringe Data"
+          main="A Power BI dashboard visualizing Boston syringe disposal data, analyzing trends in locations, disposal methods, and hotspots for better urban waste management strategies."
+          sourceLink="https://app.powerbi.com/groups/me/reports/c6f9548d-63de-4e2d-a647-850b51f7a251/9848ad5e12a4a364c4fe?experience=power-bi" 
+        />
+        <ProjectCard
+          title="Airbnb Dashboard"
+          main="An interactive Tableau dashboard analyzing Airbnb listings, focusing on price trends, room types, and host verification. Presented insights to enhance decision-making."
           sourceLink="https://public.tableau.com/app/profile/abhishek.bade/viz/aly6070_dashboard/Dashboard1"
         />
         <ProjectCard
